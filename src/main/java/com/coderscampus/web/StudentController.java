67edb931027b890422e1aff3ddb174dfbafc1ff9
MiriamLaw <mimsical999@yahoo.com>
2024-12-30 18:13:19 -0600

    Started Controllers

---
+++ src/main/java/com/coderscampus/web/StudentController.java
@@ -0,0 +1,29 @@
+package com.coderscampus.web;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Home");
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String secured(Model model) {
+        model.addAttribute("pageTitle", "Dashboard");
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        // The admin page is restricted to users with ROLE_ADMIN.
+        model.addAttribute("pageTitle", "Admin Dashboard");
+        return "admin";
+    }
+
+}
